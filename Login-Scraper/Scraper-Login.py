2bd36cec3a9ac89031c25d294af556cc4e215bd5
gingapower <fabian.huber@mein.gmx>
2023-03-03 00:04:32 +0100

    Scraper-Login.py

--- Login-Scraper/Scraper-Login.py
+++ Login-Scraper/Scraper-Login.py
@@ -10,10 +10,33 @@ result = requests.get(url).text
 doc = BeautifulSoup(result, "html.parser")
 #print(doc.prettify()) #print HTML
 
-login = doc.find_all(string="Sign in")
+#print(doc)
 
+login = doc.find_all(string="Sign in") #find gives first result
+#links = doc.find_all(["a"],text="Sign in")
+print(login)
 parent = login[0].parent
-tbody = doc.tbody
+print(parent)
+#print(links)
 
-print(tbody)
+links_with_text = []
+for a in doc.find_all('a', href=True): 
+    if a.text: 
+        links_with_text.append(a['href'])
+
+#print(links_with_text)
+
+for i in links_with_text:
+    if 'SignIn' in i:
+        print(f"The word 'Sign' is found in link: {i}")
+
+
+login_link = doc.find('a', {'class': 'SignIn'})
+if login_link is not None:
+    print(f"Found login link: {login_link['href']}")
+else:
+    print("No login link found.")
+#tbody = doc. 
+
+#print(login)
 #print(parent)
