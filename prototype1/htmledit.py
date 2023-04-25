fa1880c8d916bc53451011f187f6c579e22961d4
walderl <leon.walder@edu.htl-villach.at>
2023-04-25 14:15:48 +0200

    AI Int

--- prototype1/htmledit.py
+++ prototype1/htmledit.py
@@ -1,10 +1,24 @@
 import os
 import re
 import webbrowser
+import configparser
 
 def insert_css_links(webpage):
-    folder_path = "C:\\Users\\Fabian Huber\\Desktop\\ArtiPHISHial\\artiPHISHial\\prototype1\\"+webpage
+    config = configparser.ConfigParser()
+    cwd = os.getcwd()
+    folder_path = cwd+ "\\" + webpage
     css_files = [f for f in os.listdir(folder_path) if f.endswith(".css")]
+    # Get the current working directory
+    cwd = os.getcwd()
+    #Read the config file
+    config.read('config.ini')
+    # Get the browser path
+    browser_path = config.get('browser', 'path')
+    os.environ['BROWSER'] = browser_path
+    # Define the relative path to the file
+    relative_path = webpage + "\\index.html"
+    # Combine the current working directory and the relative path
+    file_path = os.path.join(cwd, relative_path)
 
     for css_file in css_files:
         css_filename = os.path.basename(css_file)
@@ -15,19 +29,6 @@ def insert_css_links(webpage):
         with open(html_file_path, "w", encoding='utf-8') as f:
             f.write(file_content)
     
-    os.environ['BROWSER'] = 'C:/Program Files/Mozilla Firefox/firefox.exe'
-
-    cwd = os.getcwd()
-
-    # Define the relative path to the file
-    relative_path = webpage + "index.html"
-
-    # Combine the current working directory and the relative path
-    file_path = os.path.join(cwd, relative_path)
-
-    # Use the file path in your code
     url = f"file://{file_path}"
-
     print(url)
     webbrowser.open(url)
-
