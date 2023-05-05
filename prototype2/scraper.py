93e8f43d2a572d6a842a5338b75cf65f1a21ee7a
gingapower <fabian.huber@mein.gmx>
2023-05-05 10:40:40 +0200

    .\prototype2\

--- prototype2/scraper.py
+++ prototype2/scraper.py
@@ -47,7 +47,16 @@ with open (urlpath ,"w") as file:
 
 #Scrape Page:
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+    'Referer': 'https://www.example.com',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',  # Specify accepted encodings
+    'Connection': 'keep-alive',  # Keep the connection alive
+    'Cache-Control': 'max-age=0',  # Specify caching behavior
+    'Upgrade-Insecure-Requests': '1',  # Signal support for upgrading to HTTPS
+    'DNT': '1',  # Enable Do Not Track
+    'X-Requested-With': 'XMLHttpRequest',  # Indicate AJAX request
+    # Add more headers as required
 }
 result = requests.get(url, headers=headers).text
 doc = BeautifulSoup(result, "html.parser")
