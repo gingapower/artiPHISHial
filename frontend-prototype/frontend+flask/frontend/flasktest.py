63f9f81b0fc609f0922b9ac66b64e44775f6d1c0
unknown <huberf@edu.htl-villach.at>
2023-05-11 14:55:26 +0200

    working backend call

--- frontend-prototype/frontend+flask/frontend/flasktest.py
+++ frontend-prototype/frontend+flask/frontend/flasktest.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/submit_data', methods=['POST'])
 def submit_data():
-    data = request.get_json()
-    input_value = data['input']
-    
-    # Process the input value or perform any desired actions
-    
-    # Return the input value as a response
-    return jsonify({'input': input_value})
+    data = request.json  # Assuming the request contains JSON data
+
+    # Process the data
+    # ...
+
+    # Return a response
+    print(data)
+    response = {'message': 'Data received successfully'}
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run()
